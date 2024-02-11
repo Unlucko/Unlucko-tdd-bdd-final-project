8a618d4db8ebba4bc627900d4041cc01e5d983db
Unlucko <cesarfsegurap.09@gmail.com>
2024-02-11 13:35:29 -0500

    Solutions

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -132,3 +132,32 @@ def step_impl(context, element_name, text_string):
     )
     element.clear()
     element.send_keys(text_string)
+
+@when(u'I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + "-btn"
+    element = context.driver.find_element_by_id(button_id)
+    element.click()
+
+@then(u'I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        ))
+    assert(found)
+
+@then(u'I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id("search_results")
+    assert(name not in element.text)
+
+@then(u'I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        ))
+    assert(found)
